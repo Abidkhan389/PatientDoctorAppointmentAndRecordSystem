d06f12efbb4de8b20fe8cc62a795e5222fdf21e3
Abidkhan732 <abid.khan@averlabz.com>
2025-02-24 16:22:59 +0500

    packages update and mediatR update in appicationservice

--- PatientDoctor.Application/ApplicationServiceRegistration.cs
+++ PatientDoctor.Application/ApplicationServiceRegistration.cs
@@ -1,19 +1,14 @@
 ﻿using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks; 
+using System.Reflection; 
 namespace PatientDoctor.Application
 {
     public static class ApplicationServiceRegistration
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             return services;
         }

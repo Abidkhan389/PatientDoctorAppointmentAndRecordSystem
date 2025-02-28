9a3420ea5f5b25d342c6b9b615c502164b9eec88
Farhat Ullah <farhatkhanwazir@gmail.com>
2025-02-28 11:27:23 +0500

    medicinepotency

--- PatientDoctor.Application/Features/Medicinetype/Commands/AddEditMedicineType/AddEditMedicineTypeCommand.cs
+++ PatientDoctor.Application/Features/Medicinetype/Commands/AddEditMedicineType/AddEditMedicineTypeCommand.cs
@@ -12,6 +12,6 @@ namespace PatientDoctor.Application.Features.Medicinetype.Commands.AddEditMedici
     {
         public Guid? MedicineTypeId { get; set; }
         public string TypeName { get; set; }
-        public string? TabletMg { get; set; }
+        public List<string>? MedicinePotency { get; set; } = new List<string>(); // Optional
     }
 }
---
+++ PatientDoctor.Migrations/Migrations/20250228062550_medicinepotency.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PatientDoctor.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class medicinepotency : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TabletMg",
+                schema: "Admin",
+                table: "MedicineType");
+
+            migrationBuilder.CreateTable(
+                name: "MedicinePotency",
+                schema: "Admin",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Potency = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MedicineTypeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MedicinePotency", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MedicinePotency_MedicineType_MedicineTypeId",
+                        column: x => x.MedicineTypeId,
+                        principalSchema: "Admin",
+                        principalTable: "MedicineType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MedicinePotency_MedicineTypeId",
+                schema: "Admin",
+                table: "MedicinePotency",
+                column: "MedicineTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MedicinePotency",
+                schema: "Admin");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TabletMg",
+                schema: "Admin",
+                table: "MedicineType",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}

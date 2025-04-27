36b5765ed2295717777263c18ca88cf6684e436f
Farhat Ullah <farhatkhanwazir@gmail.com>
2025-04-27 13:25:31 +0500

    hangfire

---
+++ PatientDoctor.Infrastructure/Repositories/ReminderSchedulers/ReminderSchedulerJob.cs
@@ -0,0 +1,26 @@
+﻿using Hangfire;
+using Microsoft.Extensions.DependencyInjection;
+using PatientDoctor.Application.Contracts.Persistance.ReminderService;
+using PatientDoctor.Application.Helpers.EmailRequest;
+using PatientDoctor.Infrastructure.Persistance;
+
+namespace PatientDoctor.Infrastructure.Repositories.ReminderSchedulers;
+
+public class ReminderSchedulerJob
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public ReminderSchedulerJob(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public void Execute()
+    {
+        using (var scope = _serviceProvider.CreateScope())
+        {
+            var reminderScheduler = scope.ServiceProvider.GetRequiredService<ReminderScheduler>();
+            reminderScheduler.ScheduleReminders();
+        }
+    }
+}
